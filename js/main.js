c50e6c8972d5aa30482909c6ff6f78872b51dddb
Van Meerbergen Renaud <113695652+VanMeerbergenRenaud@users.noreply.github.com>
2022-12-03 13:18:35 +0100

    ajout de la croix de retour

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
+/* -------------------------------------------
+Nom Complet: 	Renaud Van Meerbergen
+------------------------------------------- */
 
+/*1.1 Ajout d'un écouteur d'évènement au clic du lien <a> en savoir plus </a>*/
 const openLink = document.getElementById('openLink');
 const answer = document.getElementById('answer');
-const svg = document.querySelectorAll('.is-opened svg');
 
 openLink.addEventListener('click', () => {
-    /*if (answer.classList.contains("is-opened")) {
-        answer.classList.remove('is-opened');
-    } else {
-        answer.classList.add('is-opened');
-        answer.style.display = 'block';
-    }*/
     answer.classList.toggle('is-opened');
     answer.style.display = 'block';
+});
+/*1.2 Ajout d'un écouteur d'évènement au <svg> = croix X >*/
+const retour = document.getElementById('retour');
+retour.addEventListener('click', () => {
+    answer.classList.toggle('is-opened');
+    answer.style.display = 'none';
 })
+
+/*2. Ajout d'un écouteur d'évènement à ...*/
