543524a4e48003372bfd88bb691517746e02dbac
Van Meerbergen Renaud <113695652+VanMeerbergenRenaud@users.noreply.github.com>
2023-01-04 21:22:18 +0100

    utilisation d'une fonction
    Utilisation unique de 2 écouteurs d'évènements qui permettent de rajouter simplement un 'id' pour répéter la même action plusieurs fois sur d'autres lien ou svg

--- js/main.js
+++ js/main.js
@@ -4,38 +4,24 @@ Nom Complet: 	Renaud Van Meerbergen
 
 document.documentElement.classList.add("js-enabled");
 
-/*1.1 Ajout d'un écouteur d'évènement au clic du lien <a> en savoir plus </a>*/
-const openLink = document.getElementById('openLink');
-const answer = document.getElementById('answer');
+function toggleAnswer(answerId, openLinkId, retourId) {
+    const openLink = document.getElementById(openLinkId);
+    const answer = document.getElementById(answerId);
 
-openLink.addEventListener('click', () => {
-    answer.classList.toggle('is-opened');
-    answer.style.display = 'block';
-    document.body.style.backgroundColor = 'rgba(35,35,35,0.65)';
-});
+    openLink.addEventListener('click', () => {
+        answer.classList.toggle('is-opened');
+        answer.style.display = 'block';
+        document.body.style.backgroundColor = 'rgba(35,35,35,0.65)';
+    });
 
-/*1.2 Ajout d'un écouteur d'évènement au <svg> = croix X >*/
-const retour = document.getElementById('retour');
-retour.addEventListener('click', () => {
-    answer.classList.toggle('is-opened');
-    answer.style.display = 'none';
-    document.body.style.backgroundColor = "white";
-});
+    const retour = document.getElementById(retourId);
+    retour.addEventListener('click', () => {
+        answer.classList.toggle('is-opened');
+        answer.style.display = 'none';
+        document.body.style.backgroundColor = "white";
+    });
+}
 
-/*2. Ajout d'un écouteur d'évènement au lien <a> n°2 */
-const openLink2 = document.getElementById('openLink2');
-const answer2 = document.getElementById('answer2');
+toggleAnswer('answer', 'openLink', 'retour');
+toggleAnswer('answer2', 'openLink2', 'retour2');
 
-openLink2.addEventListener('click', () => {
-    answer2.classList.toggle('is-opened');
-    answer2.style.display = 'block';
-    document.body.style.backgroundColor = 'rgba(35,35,35,0.65)';
-});
-
-/*1.2 Ajout d'un écouteur d'évènement au <svg> = croix X >*/
-const retour2 = document.getElementById('retour2');
-retour2.addEventListener('click', () => {
-    answer2.classList.toggle('is-opened');
-    answer2.style.display = 'none';
-    document.body.style.backgroundColor = "white";
-});
