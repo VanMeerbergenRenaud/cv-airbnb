eae880ed137d90e3393e62c33977d83da1855bed
Renaud Van Meerbergen <renaud.vanmeerbergen@gmail.com>
2023-01-15 10:29:53 +0100

    Initial commit

--- js/calendar.js
+++ js/calendar.js
@@ -52,8 +52,63 @@
             this.initConst();
             this.renderCalendar();
             this.addEventListener();
-            document.documentElement.add.classList('js-enabled');
         }
     }
     app.init();
 }) ();
+
+/*
+Court version :
+
+(function() {
+  const app = {
+    init() {
+      this.daysTag = document.querySelector('.days');
+      this.currentDate = document.querySelector('.current-date');
+      this.prevIcon = document.querySelector('#previous');
+      this.nextIcon = document.querySelector('#next');
+      this.date = new Date();
+      this.year = this.date.getFullYear();
+      this.month = this.date.getMonth();
+      this.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+      this.prevIcon.addEventListener('click', () => this.update(-1));
+      this.nextIcon.addEventListener('click', () => this.update(1));
+      this.renderCalendar();
+      document.documentElement.classList.add('js-enabled');
+    },
+    update(offset) {
+      this.month += offset;
+      if (this.month < 0 || this.month > 11) {
+        this.year += Math.sign(offset);
+        this.month = (this.month + 12) % 12;
+      }
+      this.date = new Date(this.year, this.month);
+      this.renderCalendar();
+    },
+    renderCalendar() {
+      const firstDay = new Date(this.year, this.month, 1).getDay();
+      const lastDate = new Date(this.year, this.month + 1, 0).getDate();
+      const lastDay = new Date(this.year, this.month, lastDate).getDay();
+      const lastDateLastMonth = new Date(this.year, this.month, 0).getDate();
+      let liTag = '';
+
+      for (let i = firstDay; i > 0; i--) {
+        liTag += `<li class="inactive">${lastDateLastMonth - i + 1}</li>`;
+      }
+      for (let i = 1; i <= lastDate; i++) {
+        const isToday = i === this.date.getDate() && this.month === new Date().getMonth()
+          && this.year === new Date().getFullYear();
+        liTag += `<li class="${isToday ? 'active' : ''}">${i}</li>`;
+      }
+      for (let i = lastDay; i < 6; i++) {
+        liTag += `<li class="inactive">${i - lastDay + 1}</li>`
+      }
+
+      this.currentDate.innerText = `${this.months[this.month]} ${this.year}`;
+      this.daysTag.innerHTML = liTag;
+    },
+  };
+  app.init();
+})();
+*/
